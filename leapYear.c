a2af851546d814ae34bccb89ae5d3c60573789b4
Abbas Khan <ak5604178gmail.com>
2023-10-22 11:54:21 -0700

    leapyear

--- leapYear.c
+++ leapYear.c
@@ -59,8 +59,41 @@ int main(int argc, char **argv) {
     numPassed = numPassed + 1;
   }
 
-  //TODO: write *at least* 3 more of your own
+  //TODO: write at least 3 more of your own
   //      test cases here, they should all pass!
+year=80;
+printf("The Case 4: year = %d : ", year);
+if(!isLeapyear(year)){
+  printF("Failed!\n");
+  numFailed = numFailed + 1;
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+
+
+  year=1200
+  printf("Test Case 5: year = %d: ",year);
+  if(!isLeapYear(year)){
+    printf("Failed!\n");
+    numFailed = numFailed + 1
+  } else {
+    printf("PASSED!\n");
+    numPassed = numPassed +1;
+  }
+
+year=1300
+printf("Test Case 6: year = %d:", year);
+if(isLeapYear(year)) {
+  printf("FAILED!\n");
+  numFailed = numFailed + 1;
+} else {
+  printf("PASSED!\n");
+  numPassed = numPassed + 1
+}// three cases added
+
+
 
   printf("\n\n");
   printf("Summary:\n");
@@ -80,4 +113,11 @@ int isLeapYear(int year) {
   //      The year is stored in the variable year
   //      Your function should return true (1) if it represents a leap year
   //      and false (0) if it does not.
+  if((year%4==0 && year%100!=0) || year%400==0){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
+j
